b55e33a30f1adbf183d0bbe066d519111013d9df
Leo Prieto <leop22@outlook.fr>
2022-06-01 16:05:50 +0200

    Restaurants list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants = [
+  { id: 1,
+    name: 'Le Petit Carnot',
+    address: '13 Bd Carnot, 06560 Valbone',
+    phone_number: '+33612180271',
+    category: 'italian' },
+  { id: 2,
+    name: 'SuAndShi',
+    address: '13 Bd Jean Hibert, 06400 Cannes',
+    phone_number: '+33493303211',
+    category: 'japanese' }
+]
+
+restaurants.each do |restaurant|
+  restaurant = Restaurant.create!(restaurant)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
